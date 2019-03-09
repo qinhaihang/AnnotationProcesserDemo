cb45dddef86220a42abdf3c606d10e10a60cb28e
qinhaihang <hai449769034@qq.com>
2019-03-09 22:51:47 +0800

    1、基本实现了通过注解方式加载xml中的View

--- app/src/main/java/com/example/annotationprocesserdemo/MainActivity.java
+++ app/src/main/java/com/example/annotationprocesserdemo/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.annotationprocesserdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.example.qhh_annotation.BindView;
 import com.example.qhh_api.QhhButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    @BindView(R.id.tv_hello)
     TextView tv_hello;
 
     @Override
@@ -16,4 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         QhhButterKnife.inject(this);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("qhhqhh","textview tv_hello = " +tv_hello);
+        tv_hello.setText("Hello Annotation");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
